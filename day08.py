b11fb878929a252c339b39b3c836d90b2dbe6fad
Dan Manastireanu <danmana11@yahoo.com>
2015-12-18 22:12:52 +0200

    Day 8 part 2

--- day08.py
+++ day08.py
@@ -4,12 +4,13 @@ with open("inputs/day08.txt", "r") as file:
     data = file.readlines()
 
 n = 0
-
+n2 = 0
 
 for s in data:
-    s2 = re.sub(r'\\\\|\\"|\\x..', '!', s)
-    print s2
-    n += len(s) - len(s2) + 2
+    s1 = re.sub(r'\\\\|\\"|\\x..', '!', s)
+    s2 = re.sub(r'"|\\', '!!', s)
+    n += len(s) - len(s1) + 2
+    n2 += len(s2) - len(s) + 2
 
-
-print n;
+print('Part 1', n)
+print('Part 2', n2)
