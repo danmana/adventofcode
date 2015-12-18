2f148882722d58b7030d32cfb4dcefa1515eb1ea
Dan Manastireanu <danmana11@yahoo.com>
2015-12-18 22:31:15 +0200

    Day 17 part 2

--- day17.py
+++ day17.py
@@ -7,22 +7,35 @@ n = len(containers)
 count = 0
 eggnog = 150
 
+minFilled = n + 1
+minCount = 0
 
-def backtrace(i, sum):
+
+def backtrace(i, sum, filled):
     global count
+    global minCount
+    global minFilled
 
     if i == n:
         if sum == eggnog:
             count += 1
+            if filled < minFilled:
+                minFilled = filled
+                minCount = 1
+            elif filled == minFilled:
+                minCount += 1
         return
 
     if sum > eggnog:
         return
 
-    backtrace(i + 1, sum)
-    backtrace(i + 1, sum + containers[i])
+    # don't fill it
+    backtrace(i + 1, sum, filled)
+    # fill it
+    backtrace(i + 1, sum + containers[i], filled + 1)
 
 
-backtrace(0, 0)
+backtrace(0, 0, 0)
 
 print('Part 1', count)
+print('Part 2', minCount)
