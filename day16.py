1b80b5ad2c6cfd153d4ad4a4cd18fec29f59810e
Dan Manastireanu <danmana11@yahoo.com>
2015-12-19 01:14:01 +0200

    Day 16 part 2

--- day16.py
+++ day16.py
@@ -3,8 +3,9 @@ import json
 with open("inputs/day16.txt", "r") as file:
     data = file.readlines()
 
+
 def parseSue(s):
-    s = s[s.index(':') + 1:]
+    s = s[s.index(':') + 2:]
     s = s.replace(':', '":')
     s = s.replace(', ', ', "')
 
@@ -13,6 +14,7 @@ def parseSue(s):
 
 clue = parseSue('Clue : children: 3, cats: 7, samoyeds: 2, pomeranians: 3, akitas: 0, vizslas: 0, goldfish: 5, trees: 3, cars: 2, perfumes: 1')
 
+
 def isSue(sue):
     for prop in sue:
         if (not prop in clue) or sue[prop] != clue[prop]:
@@ -20,9 +22,25 @@ def isSue(sue):
     return True
 
 
+def isRealSue(sue):
+    for prop in sue:
+        if not prop in clue:
+            print(prop)
+            return False
+        if prop == 'cats' or prop == 'trees':
+            if sue[prop] <= clue[prop]:
+                return False
+        elif prop == 'pomeranians' or prop == 'goldfish':
+            if sue[prop] >= clue[prop]:
+                return False
+        elif sue[prop] != clue[prop]:
+            return False
+    return True
+
 
 for i in range(500):
     sue = parseSue(data[i])
     if isSue(sue):
-        print i+1
-        break
+        print('Sue', i + 1)
+    if isRealSue(sue):
+        print('Real Sue', i + 1)
