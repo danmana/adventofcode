fc5987ddee2857e081e78ea8bc9b015c6e916395
Dan Manastireanu <danmana11@yahoo.com>
2015-12-18 23:58:38 +0200

    Day 11 part 2

--- day11.py
+++ day11.py
@@ -1,28 +1,34 @@
 import re
 
 
-
-
 def rule1(s):
-    for i in range(0, len(s)-2):
-        if ord(s[i]) == ord(s[i+1]) -1 and ord(s[i]) == ord(s[i+2]) - 2:
+    for i in range(0, len(s) - 2):
+        if ord(s[i]) == ord(s[i + 1]) - 1 and ord(s[i]) == ord(s[i + 2]) - 2:
             return True
     return False
 
+
 invalid = re.compile('[ilo]')
+
+
 def rule2(s):
     return invalid.search(s) is None
 
+
 pair = re.compile(r'(\w)\1')
+
+
 def rule3(s):
     m = pair.search(s)
     if m is not None:
-        m = pair.search(s,m.end())
+        m = pair.search(s, m.end())
         return m is not None
     return False
 
+
 def isValid(s):
     return rule1(s) and rule2(s) and rule3(s)
+
 
 def nextPass(s):
     s = list(s)
@@ -40,8 +46,11 @@ def nextPass(s):
 
 
 password = "cqjxjnds"
+passCount = 0
 while True:
     password = nextPass(password)
     if isValid(password):
-        print password
-        break
+        passCount += 1
+        print('Pass', passCount, password)
+        if passCount == 2:
+            break
